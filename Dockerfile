39ca7029c91fbd275b77fa4d88e7ba6ccbb9f49a
SriHarsha Jana <sjana@yodlee.com>
2020-08-02 20:11:22 +0530

    adding docker compose

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM python:3.7.8-slim-buster
 
 ADD trec-qc-services /trec-qc-services
 RUN pip install -r trec-qc-services/requirements.txt
-RUN rm -f /root/.cache
+RUN rm -r /root/.cache
 WORKDIR /trec-qc-services
 
 EXPOSE 50051
-CMD ["python","StartGrpcSampleService.py"]
--- StartGrpcSampleClient.py
+++ StartGrpcSampleClient.py
@@ -1,16 +1,22 @@
 import grpc
+import time
+import os
 
 import proto_generated.sample_service_pb2 as service_objects
 import proto_generated.sample_service_pb2_grpc as service_stub
 
 
-def run():
-    with grpc.insecure_channel('127.0.0.1:50051') as channel:
-        stub = service_stub.QuestionClassificationServicesStub(channel)
-        request = service_objects.GetQuestionsMessage(limit=2, questionDetailsList=[])
-        response = stub.getSampleQuestions(request)
-        print(response)
+def run(channel):
+    stub = service_stub.QuestionClassificationServicesStub(channel)
+    request = service_objects.GetQuestionsMessage(limit=2, questionDetailsList=[])
+    response = stub.getSampleQuestions(request)
+    print(response)
 
 
 if __name__ == "__main__":
-    run()
+    host = os.getenv('QC-SERVER-DNS', '127.0.0.1')
+    print('connecting to host ', host)
+    with grpc.insecure_channel(host+':50051') as channel:
+        while True:
+            run(channel)
+            time.sleep(1.5)
